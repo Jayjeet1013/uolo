087d96cfb909af70f3fa9428671a63b24d958ef6
Jayjeet1013 <jayjeet1013@gmail.com>
2024-11-12 14:04:23 +0530

    chore: add Vercel Analytics integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -33,6 +34,7 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
+        <Analytics/>
         <Footer />
       </body>
     </html>
